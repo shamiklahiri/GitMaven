99bf7492bb3cc207b6452697c115a07397df0bad
shamiklahiri <shamiklahiri@gmail.com>
2015-04-16 21:53:38 +0200

    commit all

---
+++ src/main/java/com/sam/test/GitMaven/App.java
@@ -0,0 +1,13 @@
+package com.sam.test.GitMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
